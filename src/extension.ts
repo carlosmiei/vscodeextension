763c378572b1225b7bc74e4d3dee5d0a97851b1f
Carlos Gonçalves <carlos.goncalves@outsystems.com>
2021-01-12 23:08:20 +0000

    sidebar boilerplate

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,19 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { HelloWorld } from './HelloWorldPanel';
+import { SidebarProvider } from './SidebarProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+	  vscode.window.registerWebviewViewProvider(
+		"vstodo-sidebar",
+		sidebarProvider
+	  )
+	);
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
