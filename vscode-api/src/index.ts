9232d3d677f4d149a4052f7ae11001ff0dd472cb
Carlos Gonçalves <carlos.goncalves@outsystems.com>
2021-01-27 22:09:12 +0000

    database setup using typeorm

--- vscode-api/src/index.ts
+++ vscode-api/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
-
+import {createConnection} from 'typeorm';
+import {join} from "path";
+import { __prod__ } from './constants';
 
 const main = async () => {
+    await createConnection({
+        type: "postgres",
+        database: "todo",
+        entities: [join(__dirname, "./entities/*.*")],
+        logging: !__prod__,
+        synchronize: !__prod__
+    })
     const app = express();
     app.get("/", (_,res) => {
         res.send("hello");
